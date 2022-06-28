26273e4141250b67b460546c95acd3e841a61c15
tianbingyao22 <2242378609@qq.com>
2022-06-28 19:43:22 +0800

    完成了翻转字符串案例

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+    // 开发服务器的配置
+  // devServer:{
+  //   open:true,
+  //   host:"localhost",
+  // },
+  // 代码检查工具：禁用代码检查
+  lintOnSave:false,
 })
+
